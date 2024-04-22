f5ae421612b368aa8c8247f4f269eebf15a3025f
Jamesthe1 <jamesthe1sky@gmail.com>
2024-04-22 09:26:09 -0500

    Add missing CreateMultiple function

--- Nebula.UI/ButtonFactory.cs
+++ Nebula.UI/ButtonFactory.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
+using Nebula.Utils;
+
 namespace Nebula.UI {
     public static class ButtonFactory {
         public static UIButton Create (ButtonFactoryDatum datum) {
             return UIFactory.CreateButton (datum.buttonSize, datum.name, datum.text, datum.onClick, datum.fontSize,
                                                  datum.detail.color, datum.detail.font);
+        }
+
+        public static List<UIButton> CreateMultiple (List<ButtonFactoryDatum> data) {
+            return new List<UIButton> (data.Remap (Create));
         }
     }
 }
