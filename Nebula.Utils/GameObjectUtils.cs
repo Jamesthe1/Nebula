5aa94a785bb27b316fe806c824769e57ab187b22
Jamesthe1 <jamesthe1sky@gmail.com>
2024-04-22 10:07:56 -0500

    Add fast way to get menu

--- Nebula.Utils/GameObjectUtils.cs
+++ Nebula.Utils/GameObjectUtils.cs
@@ -15,5 +15,9 @@ namespace Nebula.Utils {
         public static GameObject GetRootObject (string name) {
             return SceneManager.GetActiveScene ().GetRootGameObjects ().FirstByName<GameObject> (name);
         }
+
+        public static CUIMenu GetMenu (string path) {
+            return GetRootObject ("# CUI_2D").transform.FindChild ($"Camera/ROOT_Menus/{path}").GetComponent<CUIMenu> ();
+        }
     }
 }
