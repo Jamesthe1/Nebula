09f1a49847ab0cf77f2ad01f8dd62a5798988831
Jamesthe1 <jamesthe1sky@gmail.com>
2025-09-04 18:10:46 -0500

    Add mod config and prepatches

---
+++ Nebula.ModConfig.Prepatches/NebulaPatcher.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Mono.Cecil;
+using Nebula.EasyCecil;
+
+public class NebulaPatcher {
+    public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+    
+    public static void Patch (AssemblyDefinition asm) {
+        PatchManager patchMgr = new PatchManager (asm);
+
+        // This extra is necessary for our mod config
+        TypeDefinition menuEnum = asm.MainModule.GetType ("MenuSubstate");
+        FieldInsertion cfgEntry = new FieldInsertion (
+            menuEnum,
+            "ModConfig",
+            0x10000000,
+            FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.Public | FieldAttributes.HasDefault,
+            CtorTarget.None
+        );
+        patchMgr.PatchType (menuEnum, new List<ITypePatch> {cfgEntry});
+    }
+}
