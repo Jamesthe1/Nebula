771e903b42ad8cc6fb8aee10b7675a2cdc7deefe
Jamesthe1 <jamesthe1sky@gmail.com>
2025-09-05 15:32:41 -0500

    Enforce using boxed values in EnumGeneric

--- Nebula.ModConfig/EntryTypes/CUIModConfigEnumGenericEntry.cs
+++ Nebula.ModConfig/EntryTypes/CUIModConfigEnumGenericEntry.cs
@@ -1,11 +1,23 @@
-using System;
-using System.Collections.Generic;
-
 namespace Nebula.ModConfig.EntryTypes {
     // Using object because the underlying enum type is unknown to our project
+    // We also have to use the boxed values because we cannot cast objects of a different template argument (So ConfigEntry<MyEnum> can't be casted to ConfigEntry<object>)
+    // Indubitably messy but it's what works
     public class CUIModConfigEnumGenericEntry : CUIModConfigCycleEntry<object> {
-        internal void PushEntry (object enumValue) {
+        protected override void CacheCurrentValue () {
+            data.BoxedValue = value;
+        }
+
+        protected override void OnEnable () {
+            value = data.BoxedValue;
+            RefreshButton ();
+        }
+
+        public void PushEntry (object enumValue) {
             cycle.Add (enumValue);
+        }
+
+        protected override string GetValueStringUnformatted () {
+            return value.ToString ().ToUpper ();
         }
     }
 }
